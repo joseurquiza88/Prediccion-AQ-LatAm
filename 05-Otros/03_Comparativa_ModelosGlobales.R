718b4c976a392dd4acfe05bcfcfdf8b4bf3898d3
= <=>
2025-11-10 12:29:28 -0300

    Modelos globales

--- 05-Otros/03_Comparativa_ModelosGlobales.R
+++ 05-Otros/03_Comparativa_ModelosGlobales.R
@@ -14,19 +14,21 @@
 # https://github.com/pmbusch/PM25-Satellite-Chile/tree/main
 
 # Sitio
-estacion <- "BA"
+estacion <- "CH"
 
 #year<-2022
 # Archivo donde estan los nombres de las estciones y las coordenadas
 data_estacciones <- read.csv(paste("D:/Josefina/Proyectos/ProyectoChile/",estacion,"/dataset/estaciones/sitios_",estacion,".csv",sep=""))
 data_estacciones <- data_estacciones[data_estacciones$Considerado=="SI",]
-#data_estacciones <- data_estacciones[data_estacciones$tipo=="referencia",]
+data_estacciones <- data_estacciones[data_estacciones$tipo=="referencia",]
 # el csv tiene una columna que se llama long y otra lat
 monitors<- data_estacciones[, c("long", "lat")]
 
 # directorio donde estan los archivos .nc
 #dir <- paste("D:/Josefina/Proyectos/ProyectoChile/",estacion,"/Comparativas_resultados/PM_WEI/",year,"/",sep="")
-dir <- "D:/Josefina/Proyectos/ProyectoChile/ModelosGlobales/WUSTL/"
+modeloGlobal <- "WEI"# WUSTL
+dir <- paste("D:/Josefina/Proyectos/ProyectoChile/ModelosGlobales/",modeloGlobal,"/",sep="")
+
 setwd(dir)
 # Lista de los archivos en formato .nc
 id <- list.files(path = dir,
@@ -37,7 +39,8 @@ id <- list.files(path = dir,
 print(length(id))
 
 df_rbind <- data.frame()
-
+df_rbind_bi <- data.frame()
+i<-1
 # Recorro toda la carpeta con los archivos .nc y extriago los datos 
 # en las coordenadas de las estaciones
 for (i in 1:length(id)){
@@ -48,40 +51,55 @@ for (i in 1:length(id)){
   mp <- raster(file)
   #mp_chile <- crop(mp, extent(-76,-66, -54.85, -17.5))
   # Extraccion valores de las estaciones
-  extracted_values <- raster::extract(mp, monitors, method="bilinear") # bilinear interpolates from the four nearest cell
-  
+  extracted_values <- raster::extract(mp, monitors)#, method="bilinear") # bilinear interpolates from the four nearest cell
+  extracted_values_bi <- raster::extract(mp, monitors, method="bilinear")
   ## Unir datos de concentraciones con los datos de extraidos
   puntos_con_valores <- data_estacciones %>%
     mutate(extracted_values = extracted_values)
+  
+  puntos_con_valores_bi <- data_estacciones %>%
+    mutate(extracted_values_bi = extracted_values_bi)
   # info extra para agregar al dataframe
-  puntos_con_valores$monthYear <- substr(file, 26,31)
-  puntos_con_valores$producto <- substr(file, 1,6)
+  ##--- WUSTL
+  # puntos_con_valores$monthYear <- substr(file, 26,31)
+  # puntos_con_valores$producto <- substr(file, 1,6)
+  
+  ##--- WEI
+  puntos_con_valores$monthYear <- substr(file, 16,21)
+  puntos_con_valores$producto <- substr(file, 1,10)
+  puntos_con_valores_bi$monthYear <- substr(file, 16,21)
+  puntos_con_valores_bi$producto <- substr(file, 1,10)
+  
   df_rbind <- rbind(df_rbind,puntos_con_valores)
+  df_rbind_bi <- rbind(df_rbind_bi,puntos_con_valores_bi)
 }
 View(df_rbind)
 
 #Guardamos csv
-save_dir <- paste("D:/Josefina/Proyectos/ProyectoChile/",estacion,"/Comparativas_resultados/PM_WEI/",sep="")
-#write.csv(df_rbind,paste(save_dir,"data_PM-WUSTL-",year,".csv",sep=""))
-write.csv(df_rbind,paste(save_dir,"data_PM-WEI_",estacion,".csv",sep=""))
+save_dir <- paste("D:/Josefina/Proyectos/ProyectoChile/",estacion,"/Comparativas_resultados/PM_",modeloGlobal,"/",sep="")
+
+write.csv(df_rbind,paste(save_dir,"data_PM-",modeloGlobal,"_",estacion,".csv",sep=""))
 setwd(save_dir)
 
+write.csv(df_rbind_bi,paste(save_dir,"data_PM-",modeloGlobal,"_",estacion,"_BI.csv",sep=""))
+setwd(save_dir)
 
 ############################################################################
 ############################################################################
 ## Unimos con datos de monitoreo de los sitios
 # Se hace una validacion con los sitios de moniteoro
-estacion <- "BA"
+estacion <- "CH"
+modeloGlobal <- "WEI"# WUSTL
 # Datos extraido de los mapas propios generados. Estan diarios hacer media mensual
 df_modelado <- read.csv(paste("D:/Josefina/Proyectos/ProyectoChile/",estacion,"/Comparativas_resultados/PM_modelado/data_PM-Modelado-TOT_",estacion,".csv",sep=""))
 df_modelado$date <- as.Date(df_modelado$date,format = "%Y-%m-%d")
-df_WUSTL <- read.csv(paste("D:/Josefina/Proyectos/ProyectoChile/",estacion,"/Comparativas_resultados/PM_WEI/data_PM-WEI_",estacion,".csv",sep=""))
+df_modeloGlobal <- read.csv(paste("D:/Josefina/Proyectos/ProyectoChile/",estacion,"/Comparativas_resultados/PM_",modeloGlobal,"/data_PM-",modeloGlobal,"_",estacion,".csv",sep=""))
 
 # Promedios mensuales por estacion
 df_estaciones <- read.csv(paste("D:/Josefina/Proyectos/ProyectoChile/",estacion,"/proceed/06_estaciones/",estacion,"_estaciones.csv",sep=""))
 df_estaciones$date <- as.Date(df_estaciones$date,format = "%d/%m/%Y")#"%Y-%M-%d")#
 # solo para CH
-#df_estaciones$mean <- df_estaciones$Registros.completos
+df_estaciones$mean <- df_estaciones$Registros.completos
 # Agrupamos datos por mes
 df_estaciones_mes <- df_estaciones %>%
   mutate(mes = floor_date(date, "month")) %>%  
@@ -103,7 +121,7 @@ df_estaciones_mes$date <- as.Date(df_estaciones_mes$mes,format = "%Y-%m-%d")
 #df_estaciones_mes<- df_estaciones_mes[complete.cases(df_estaciones_mes$mes),]
 ###
 #Setear formato de dataframe mensual del modelo global
-df_WUSTL$date <- as.Date(paste0(df_WUSTL$monthYear, "01"), format = "%Y%m%d")
+df_modeloGlobal$date <- as.Date(paste0(df_modeloGlobal$monthYear, "01"), format = "%Y%m%d")
 # df_WEI$date <- as.Date(df_WEI$date,format = "%Y-%m-%d")
 df_modelado_mes$date <- as.Date(df_modelado_mes$mes,format = "%Y-%m-%d")
 
@@ -114,26 +132,26 @@ df_merge_modelado_Estacion$date<-as.Date(df_merge_modelado_Estacion$date,format 
 
 ####
 #Merge datos anteriores vs el modelo global por dia y estaciones
-df_merge_WUSTLModEstacion <- merge(df_merge_modelado_Estacion, df_WUSTL, by = c("date", "estacion"), all.x = TRUE)
-df_merge_WUSTLModEstacion <- df_merge_WUSTLModEstacion[complete.cases(df_merge_WUSTLModEstacion$extracted_values),]
-df_merge_WUSTLModEstacion_comp <- df_merge_WUSTLModEstacion[complete.cases(df_merge_WUSTLModEstacion$media.x),]
-df_merge_WUSTLModEstacion_comp <- df_merge_WUSTLModEstacion_comp[complete.cases(df_merge_WUSTLModEstacion_comp$media.y),]
+df_merge_modeloGlobalModEstacion <- merge(df_merge_modelado_Estacion, df_modeloGlobal, by = c("date", "estacion"), all.x = TRUE)
+df_merge_modeloGlobalModEstacion <- df_merge_modeloGlobalModEstacion[complete.cases(df_merge_modeloGlobalModEstacion$extracted_values),]
+df_merge_modeloGlobalModEstacion_comp <- df_merge_modeloGlobalModEstacion[complete.cases(df_merge_modeloGlobalModEstacion$media.x),]
+df_merge_modeloGlobalModEstacion_comp <- df_merge_modeloGlobalModEstacion_comp[complete.cases(df_merge_modeloGlobalModEstacion_comp$media.y),]
 # Mejoramos el dataframe
-df_merge_WUSTLModEstacion <- data.frame(date = df_merge_WUSTLModEstacion_comp$date,
-                                     estacion = df_merge_WUSTLModEstacion_comp$estacion,
-                                     mediciones = df_merge_WUSTLModEstacion_comp$media.y,
-                                     Model = df_merge_WUSTLModEstacion_comp$media.x,
-                                     WUSTL = df_merge_WUSTLModEstacion_comp$extracted_values)
+df_merge_modeloGlobalModEstacion <- data.frame(date = df_merge_modeloGlobalModEstacion_comp$date,
+                                     estacion = df_merge_modeloGlobalModEstacion_comp$estacion,
+                                     mediciones = df_merge_modeloGlobalModEstacion_comp$media.y,
+                                     Model = df_merge_modeloGlobalModEstacion_comp$media.x,
+                                     modeloGlobal = df_merge_modeloGlobalModEstacion_comp$extracted_values)
 #Vemos si hay datos faltantes
-df_merge_WUSTLModEstacion <- df_merge_WUSTLModEstacion [complete.cases(df_merge_WUSTLModEstacion),]
+df_merge_modeloGlobalModEstacion <- df_merge_modeloGlobalModEstacion [complete.cases(df_merge_modeloGlobalModEstacion),]
 
 # Asegurar que el data frame sea tibble
-df_merge_WUSTLModEstacion <- as_tibble(df_merge_WUSTLModEstacion)
+df_merge_modeloGlobalModEstacion <- as_tibble(df_merge_modeloGlobalModEstacion)
 
-# Regresiones lineales: Model ~ SINCA y WUSTL ~ Mediciones
-modelo1 <- lm(Model ~ mediciones, data = df_merge_WUSTLModEstacion)
-modelo2 <- lm(WUSTL ~ mediciones, data = df_merge_WUSTLModEstacion)
-modelo3 <- lm(Model~ WUSTL, data = df_merge_WUSTLModEstacion)
+# Regresiones lineales: Model ~ SINCA y modeloGlobal ~ Mediciones
+modelo1 <- lm(Model ~ mediciones, data = df_merge_modeloGlobalModEstacion)
+modelo2 <- lm(modeloGlobal ~ mediciones, data = df_merge_modeloGlobalModEstacion)
+modelo3 <- lm(Model~ modeloGlobal, data = df_merge_modeloGlobalModEstacion)
 r2_modelo1 <- summary(modelo1)$r.squared
 r2_modelo2 <- summary(modelo2)$r.squared
 r2_modelo3 <- summary(modelo3)$r.squared
@@ -151,12 +169,12 @@ pred_1 <- predict(modelo1)
 pred_2 <- predict(modelo2)
 pred_3 <- predict(modelo3)
 # Calcular Bias y RMSE para cada modelo
-bias_1 <- mean(pred_1 - df_merge_WUSTLModEstacion$Model)
-bias_2 <- mean(pred_2 - df_merge_WUSTLModEstacion$WUSTL)
+bias_1 <- mean(pred_1 - df_merge_modeloGlobalModEstacion$Model)
+bias_2 <- mean(pred_2 - df_merge_modeloGlobalModEstacion$modeloGlobal)
 
-rmse_1 <- sqrt(mean((pred_1 - df_merge_WUSTLModEstacion$Model)^2))
-rmse_2 <- sqrt(mean((pred_2 - df_merge_WUSTLModEstacion$WUSTL)^2))
-rmse_3 <- sqrt(mean((pred_3 - df_merge_WUSTLModEstacion$WUSTL)^2))
+rmse_1 <- sqrt(mean((pred_1 - df_merge_modeloGlobalModEstacion$Model)^2))
+rmse_2 <- sqrt(mean((pred_2 - df_merge_modeloGlobalModEstacion$modeloGlobal)^2))
+rmse_3 <- sqrt(mean((pred_3 - df_merge_modeloGlobalModEstacion$modeloGlobal)^2))
 
 rmse_1
 rmse_2
@@ -170,29 +188,31 @@ rmse_3
 ############################################################################
 ############################################################################
 #### Validacion con el raster completo pixel a pixel
-
+modeloGlobal <- "WEI"# WUSTL
 #Path con todos los archivos del modelo global
-dir_WUSTL <- "D:/Josefina/Proyectos/ProyectoChile/ModelosGlobales/WUSTL/"
-setwd(dir_WUSTL)
+dir_modeloGlobal <- paste("D:/Josefina/Proyectos/ProyectoChile/ModelosGlobales/",modeloGlobal,"/",sep="")
+setwd(dir_modeloGlobal)
 
 # Lista de archivos .nc
-id_WUSTL <- list.files(path = dir_WUSTL,
+id_modeloGlobal <- list.files(path = dir_modeloGlobal,
                        pattern = "*.nc",
                        full.names = FALSE)
 
-cat("Cantidad de archivos WUSTL:", length(id_WUSTL), "\n")
+cat("Cantidad de archivos modeloGlobal:", length(id_modeloGlobal), "\n")
 
 # Extraer fechas del nombre (formato YYYYMM)
-fechas_WUSTL <- substr(id_WUSTL, 26, 31)
-date_WUSTL <- as.Date(paste0(fechas_WUSTL, "01"), format = "%Y%m%d")
+# fechas_modeloGlobal <- substr(id_modeloGlobal, 26, 31) WUSTL
+fechas_modeloGlobal <- substr(id_modeloGlobal, 16, 21) #WEI
+
+date_modeloGlobal <- as.Date(paste0(fechas_modeloGlobal, "01"), format = "%Y%m%d")
 
 # Crear dataframe con nombre y fecha
-df_WUSTL <- data.frame(archivo_WUSTL = id_WUSTL, fecha_WUSTL = date_WUSTL)
+df_modeloGlobal <- data.frame(archivo_modeloGlobal = id_modeloGlobal, fecha_modeloGlobal = date_modeloGlobal)
 
 # ---------------------------------------------------------------
 #Modelo propio por centro urbano
-estacion <- "BA"
-modelo <- "01-ET-CV-M1-170625-BA"
+estacion <- "CH"
+modelo <- "01-XGB-CV-M1-190625-CH"
 # Directorio con los valores mensuales del modelo selccionado
 dir_modelo <- paste0("D:/Josefina/Proyectos/ProyectoChile/", estacion,
                      "/modelos/salidas/SalidasMensuales/", modelo, "/")
@@ -217,8 +237,8 @@ df_modelo <- data.frame(archivo_modelo = id_modelo, fecha_modelo = date_modelo)
 # Como no estan en el mismo orden, hay que emparejar los archivos 
 # fecha
 
-df_match <- inner_join(df_modelo, df_WUSTL,
-                       by = c("fecha_modelo" = "fecha_WUSTL"))
+df_match <- inner_join(df_modelo, df_modeloGlobal,
+                       by = c("fecha_modelo" = "fecha_modeloGlobal"))
 
 cat("Fechas coincidentes encontradas:", nrow(df_match), "\n")
 print(df_match)
@@ -235,21 +255,21 @@ for (i in 1:nrow(df_match)) {
   
   # Leer los rasters 
   raster_modelo <- raster(paste0(dir_modelo, df_match$archivo_modelo[i]))
-  raster_WUSTL <- raster(paste0(dir_WUSTL, df_match$archivo_WUSTL[i]))
+  raster_modeloGlobal <- raster(paste0(dir_modeloGlobal, df_match$archivo_modeloGlobal[i]))
   
-  # Recortar WUSTL al extent del modelo
-  raster_WUSTL_recortado <- crop(raster_WUSTL, extent(raster_modelo))
+  # Recortar modeloGlobal al extent del modelo
+  raster_modeloGlobal_recortado <- crop(raster_modeloGlobal, extent(raster_modelo))
   
-  # Ajustar resolución (bilinear para valores continuos)
-  raster_WUSTL_resample <- resample(raster_WUSTL_recortado, raster_modelo, method = "bilinear")
+  # Ajustar resoluci?n (bilinear para valores continuos)
+  raster_modeloGlobal_resample <- resample(raster_modeloGlobal_recortado, raster_modelo, method = "bilinear")
   
   # Convertir ambos en data frame
-  df <- as.data.frame(stack(raster_modelo, raster_WUSTL_resample), xy = FALSE, na.rm = TRUE)
-  colnames(df) <- c("modelo", "WUSTL")
+  df <- as.data.frame(stack(raster_modelo, raster_modeloGlobal_resample), xy = FALSE, na.rm = TRUE)
+  colnames(df) <- c("modelo", "modeloGlobal")
   
   # Agregar columnas con las fechas de ambos modelos
   df$fecha_modelo <- df_match$fecha_modelo[i]
-  # df$fecha_WUSTL <- df_match$fecha_WUSTL[i]
+  # df$fecha_modeloGlobal <- df_match$fecha_modeloGlobal[i]
   
   # Agregar al dataset total
   df_rbind <- rbind(df_rbind, df)
@@ -258,18 +278,18 @@ for (i in 1:nrow(df_match)) {
 
 
 # ---------------------------------------------------------------
-# Calcular métricas para el mes
-fit <- lm(modelo ~ WUSTL, data = df_rbind)
+# Calcular m?tricas para el mes
+fit <- lm(modelo ~ modeloGlobal, data = df_rbind)
 r2 <- summary(fit)$r.squared
-rmse <- sqrt(mean((df_rbind$modelo - df_rbind$WUSTL)^2))
-cor(x=df_rbind$modelo, y=df_rbind$WUSTL)
+rmse <- sqrt(mean((df_rbind$modelo - df_rbind$modeloGlobal)^2))
+cor(x=df_rbind$modelo, y=df_rbind$modeloGlobal)
 r2
 rmse
 
 # ---------------------------------------------------------------
 #Guardar csv
 dir_resultados <- "D:/Josefina/Proyectos/ProyectoChile/ModelosGlobales/resultados/"
-write.csv(df_rbind, paste(dir_resultados,"comparativa_pixel_",estacion, ".csv",sep=""))
+write.csv(df_rbind, paste(dir_resultados,"comparativa_pixel_",estacion,"_",modeloGlobal, ".csv",sep=""))
 
 
 
