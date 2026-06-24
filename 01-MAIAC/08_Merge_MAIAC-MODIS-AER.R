e5e52ce47009a0bc7d685e5b1b239f065e6f38e3
= <=>
2026-06-24 12:14:57 -0300

    docmentacion 01-MAIAC_08-MergeMAIAC

--- 01-MAIAC/08_Merge_MAIAC-MODIS-AER.R
+++ 01-MAIAC/08_Merge_MAIAC-MODIS-AER.R
@@ -1,33 +1,23 @@
-#######################################################################
-## OBJETIVO: Unir la info de MAIAC-MODIS-AERONET
-##
-#######################################################################
+# Objetivo: ----
+# Unir la info de MAIAC-MODIS-AERONET
 
-###### Ejemplo para una estacion
+# Ejemplo para una estacion ----
 rm(list = setdiff(ls(), "time_correlation"))
-
-buffer_time <- 120 #minutes
+buffer_time <- 120 #min
 buffer_spatial <- "3KM"
 city <- "SP"
 num_estacion <- 1
-for(i in 1:1){ # recorremos una vez
-  #Abrir archivos de MAIAC/MODIS
-  MODIS <- read.csv(paste("D:/Josefina/paper_git/paper_maiac/datasets/V03/processed/merge_AER-MODIS/Latam/dia/",buffer_spatial,"/",buffer_time,"mins/",num_estacion,"_",city,"-",buffer_spatial,"-MODIS-",buffer_time,"-AER_MEAN.csv",sep=""))
-  MAIAC <- read.csv(paste("D:/Josefina/paper_git/paper_maiac/datasets/V03/processed/merge_AER-MAIAC/Latam_C61/dia/",buffer_spatial,"/",buffer_time,"mins/",num_estacion,"_",city,"-",buffer_spatial,"-MAIAC-",buffer_time,"-AER_MEAN_c61.csv",sep=""))
+for (i in 1:1){ # recorremos una vez
+  MODIS <- read.csv(paste("D:/Josefina/paper_git/paper_maiac/datasets/V03/processed/merge_AER-MODIS/Latam/dia/", buffer_spatial, "/", buffer_time, "mins/", num_estacion, "_", city, "-", buffer_spatial, "-MODIS-", buffer_time, "-AER_MEAN.csv", sep = ""))
+  MAIAC <- read.csv(paste("D:/Josefina/paper_git/paper_maiac/datasets/V03/processed/merge_AER-MAIAC/Latam_C61/dia/", buffer_spatial, "/", buffer_time, "mins/", num_estacion, "_", city, "-", buffer_spatial, "-MAIAC-", buffer_time, "-AER_MEAN_c61.csv", sep = ""))
   # Formato fecha
   MODIS$date <-  as.Date(as.POSIXct(strptime(MODIS$date, format = "%Y-%m-%d %H:%M:%S", "GMT")))
-  MAIAC$date <-   as.Date(as.POSIXct(strptime(MAIAC$date, format = "%Y-%m-%d", "GMT")))
-  
+  MAIAC$date <-  as.Date(as.POSIXct(strptime(MAIAC$date, format = "%Y-%m-%d", "GMT")))
   #Merge segun la fecha
-  merge_sat <- merge(x = MAIAC, y = MODIS, by = "date") 
-  # Hay muchas columnas sin usar, armamos un nuevo df con las de interes
-  merge_sat <- data.frame(date = merge_sat$date,
-                          AOD_modis = merge_sat$AOD_550_modis,
-                          
-                          AOD_maiac_61 = merge_sat$AOD_550_maiac_mean,
-                          AOD_550_AER_mean = merge_sat$AOD_550_AER_mean.y)
-  
+  merge_sat <- merge(x = MAIAC, y = MODIS, by = "date")
+  merge_sat <- data.frame (date = merge_sat$date, AOD_modis = merge_sat$AOD_550_modis,
+                        AOD_maiac_61 = merge_sat$AOD_550_maiac_mean,
+                        AOD_550_AER_mean = merge_sat$AOD_550_AER_mean.y)
   print(nrow(merge_sat))
   # Guardar archivo
-  write.csv(merge_sat,paste("D:/Josefina/paper_git/paper_maiac/datasets/V03/processed/merge_AER-MAIAC-MODIS/Latam/dia/",buffer_spatial,"/",buffer_time,"mins/",num_estacion,"_",city,"-",buffer_spatial,"-MAIAC-MODIS-",buffer_time,"-AER_MEAN_c61.csv",sep=""))
-}
+  write.csv(merge_sat,paste("D:/Josefina/paper_git/paper_maiac/datasets/V03/processed/merge_AER-MAIAC-MODIS/Latam/dia/", buffer_spatial, "/", buffer_time , "mins/", num_estacion, "_", city, "-", buffer_spatial, "-MAIAC-MODIS-", buffer_time, "-AER_MEAN_c61.csv", sep = ""))}
